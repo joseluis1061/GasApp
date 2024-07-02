89069f6868a93c9914c409245960a8bb3f0cba43
José Luis Zúñiga <jose@tangibledesign.co>
2024-07-02 10:16:26 -0500

    Add function of authenticate user

--- src/app/common/services/fireauth.service.ts
+++ src/app/common/services/fireauth.service.ts
@@ -1,9 +1,76 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Auth, User, user } from '@angular/fire/auth';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+import { Observable, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FireauthService {
+  private auth: Auth = inject(Auth);
+  private router: Router = inject(Router);
+  user$ = user(this.auth);
+  userSubscription!: Subscription;
 
-  constructor() { }
+  constructor() {}
+
+  login(email:string, password: string){
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+  registerUser(email: string, password: string){
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  stateAuth(){
+    onAuthStateChanged(this.auth, (user) => {
+      return user; //user.uid;
+    })     
+  }
+
+  logout(){
+    signOut(this.auth).then(() => {
+      // Sign-out successful.
+      this.router.navigate(['/']);
+    }).catch((error) => {
+      // An error happened.
+      this.router.navigate(['/']);
+    });
+  }
+
 }
+
+
+/*
+registerUser(email: string, password: string): Promise<string> {
+    return new Promise((resolve) => {
+      createUserWithEmailAndPassword(this.auth, email, password)
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          return user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          return errorMessage;
+        });
+    });
+  }
+
+  login(email: string, password: string): Promise<string> {
+    return new Promise((resolve) => {
+      signInWithEmailAndPassword(this.auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    });
+  }
+*/
